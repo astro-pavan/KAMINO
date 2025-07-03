561dce2f9e3cc61372c0a627aa0ff3c1fa4228c2
Pavan <pt426@cam.ac.uk>
2025-07-03 21:15:21 +0100

    Added phreeqc install script

---
+++ scripts/install_phreeqc.sh
@@ -0,0 +1,15 @@
+cd ../external
+
+mkdir phreeqc
+cd phreeqc
+INSTALLDIR=$PWD
+cd ..
+
+# unpacks and installs PHREEQC
+tar -xvzf phreeqc-3.8.6-17100.tar.gz
+cd phreeqc-3.8.6-17100
+mkdir Release
+cd Release
+../configure --prefix=$INSTALLDIR
+make
+make install
