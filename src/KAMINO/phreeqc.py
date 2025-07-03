f969d2c90b53a810def60c18d7f5955110f69ad3
Pavan <pt426@cam.ac.uk>
2025-07-03 23:39:42 +0100

    Minor changes

--- src/KAMINO/phreeqc.py
+++ src/KAMINO/phreeqc.py
@@ -13,7 +13,7 @@ output_file_path = f'{PHREEQC_path}/output.txt'
 def find_partial_pressure(P: float, T: float, carbon_molality: float):
 
     input_template_file_path = 'templates/partial_pressure_input.txt'
-    input_file_path_new = f'{PHREEQC_path}/input_partial_pressure'
+    input_file_path_new = f'{PHREEQC_path}/input'
     wd: str = os.getcwd()
 
     input_modifications: dict[int, str] = {
@@ -40,14 +40,14 @@ def find_partial_pressure(P: float, T: float, carbon_molality: float):
 def find_carbon_molality(P: float, T: float, P_CO2: float):
 
     input_template_file_path = 'templates/molality_input.txt'
-    input_file_path_new = f'{PHREEQC_path}/input_molality'
+    input_file_path_new = f'{PHREEQC_path}/input'
     wd: str = os.getcwd()
 
     input_modifications: dict[int, str] = {
         1 : f'DATABASE {wd}/external/phreeqc-3.8.6-17100/database/phreeqc.dat',
-        4 : f'    temp        {T + ABSOLUTE_ZERO:.2f}        # Temperature in degrees Celsius',
+        4 : f'    temp        {T + ABSOLUTE_ZERO:.4f}        # Temperature in degrees Celsius',
         5 : f'    pressure    {P / EARTH_ATM:.4f}         # Pressure in atmospheres',
-        13 : f'    CO2(g) {np.log10(P_CO2 / EARTH_ATM):.2f}'
+        13 : f'    CO2(g) {np.log10(P_CO2 / EARTH_ATM):.4f}'
     }
 
     modify_file_by_lines(input_template_file_path, input_file_path_new, input_modifications)
@@ -62,13 +62,17 @@ def find_carbon_molality(P: float, T: float, P_CO2: float):
 
 if __name__ == '__main__':
 
-    molality = find_carbon_molality(1e5, 300, 0.004 * 1e5) # type: ignore
+    P_CO2_original = 0.004 * EARTH_ATM
+    molality_original = 0.002 
+
+    molality = find_carbon_molality(1e5, 300, P_CO2_original) # type: ignore
 
     print(molality)
 
-    P_CO2, P_H2O = find_partial_pressure(1e5, 300, molality) # type: ignore
+    P_CO2, P_H2O = find_partial_pressure(1e5, 300, molality_original) # type: ignore
 
-    print(P_CO2 / 1e5)
+    print(P_CO2 / EARTH_ATM)
+    print(P_H2O / EARTH_ATM)
 
 
 
