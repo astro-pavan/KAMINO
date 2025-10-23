e188104c55c12092bab08d7e9445b5c9d17bc1b5
astro-pavan <pt426@cam.ac.uk>
2025-10-23 14:21:58 +0100

    Updated phreeqc

--- src/KAMINO/external/phreeqc.py
+++ src/KAMINO/external/phreeqc.py
@@ -71,19 +71,30 @@ def reverse_partial_pressure(P: float, T: float, P_CO2: float, composition: dict
     else:
         raise ValueError("Root finding did not converge")
     
-def seafloor_equilbrium(P_seafloor:float, T_seafloor:float, composition: dict[str, float], alkalinity: Union[float, None]=None, carbon_molality: Union[float, None] = None, pH: Union[float, None]=None) -> tuple[dict[str, float], float, float]:
+def seafloor_equilbrium(P_seafloor:float, T_seafloor:float, composition: dict[str, float], minerals: list[str], alkalinity: Union[float, None]=None, carbon_molality: Union[float, None] = None, pH: Union[float, None]=None) -> tuple[dict[str, float], float, float]:
     
     input_template_file_path = 'input/seafloor_weathering_input.txt'
     input_file_path_new = f'{PHREEQC_path}/input'
     wd: str = os.getcwd()
 
+    mineral_list = ''
+    for mineral in minerals:
+        mineral_list += mineral + ' '
+
     input_modifications: dict[int, str] = {
         1 : f'DATABASE {wd}/external/phreeqc-3.8.6-17100/database/phreeqc.dat',
-        12: f'    {P_seafloor / EARTH_ATM:.4f}',
-        14: f'    {T_seafloor + ABSOLUTE_ZERO:.4f}'
+        9: f'    {P_seafloor / EARTH_ATM:.4f}',
+        11: f'    {T_seafloor + ABSOLUTE_ZERO:.4f}',
+        16: f'    -equilibrium_phases {mineral_list}',
+        17: f'    -saturation_indices {mineral_list}'
     }
 
     modify_file_by_lines(input_template_file_path, input_file_path_new, input_modifications)
+
+    input_newlines: list[str] = []
+    for mineral in minerals:
+        input_newlines.append(f'    {mineral}   0.0 10.0')
+    insert_lines_into_file(input_file_path_new, input_newlines, 6)
 
     input_newlines: list[str] = []
 
@@ -97,7 +108,64 @@ def seafloor_equilbrium(P_seafloor:float, T_seafloor:float, composition: dict[st
     for k in composition.keys():
         input_newlines.append(f'    {k}    {composition[k]:.8f}')
 
-    insert_lines_into_file(input_file_path_new, input_newlines, 5)
+    insert_lines_into_file(input_file_path_new, input_newlines, 4)
+
+    subprocess.run(['./phreeqc', 'input'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=PHREEQC_path)
+
+    solution_df: pd.DataFrame = pd.read_table(output_file_path, sep='\s+') # type: ignore
+
+    # print(solution_df.keys())
+
+    new_composition: dict[str, float]= {}
+
+    for k in composition:
+        new_composition[k] = float(np.array(solution_df[k])[1]) # type: ignore
+
+    new_alkalinity = float(np.array(solution_df['Alkalinity'])[1]) # type: ignore
+    new_carbon_molality = float(np.array(solution_df['C'])[1]) # type: ignore
+
+    return new_composition, new_alkalinity, new_carbon_molality
+
+def seafloor_equilbrium_v2(P_seafloor:float, T_seafloor:float, composition: dict[str, float], minerals: list[str], alkalinity: Union[float, None]=None, carbon_molality: Union[float, None] = None, pH: Union[float, None]=None) -> tuple[dict[str, float], float, float]:
+    
+    input_template_file_path = 'input/seafloor_weathering_input_v2.txt'
+    input_file_path_new = f'{PHREEQC_path}/input'
+    wd: str = os.getcwd()
+
+    mineral_list = ''
+    for mineral in minerals:
+        mineral_list += mineral + ' '
+
+    #external/phreeqc-3.8.6-17100/database/Kinec_v3.dat
+
+    input_modifications: dict[int, str] = {
+        1 : f'DATABASE {wd}/external/phreeqc-3.8.6-17100/database/Kinec_v3.dat',
+        6: f'    pressure {P_seafloor / EARTH_ATM:.4f}',
+        5: f'    temp {T_seafloor + ABSOLUTE_ZERO:.4f}',
+        16: f'    -equilibrium_phases {mineral_list}',
+        17: f'    -saturation_indices {mineral_list}'
+    }
+
+    modify_file_by_lines(input_template_file_path, input_file_path_new, input_modifications)
+
+    input_newlines: list[str] = []
+    for mineral in minerals:
+        input_newlines.append(f'    {mineral}   0.0 10.0')
+    insert_lines_into_file(input_file_path_new, input_newlines, 8)
+
+    input_newlines: list[str] = []
+
+    if pH is not None:
+        input_newlines.append(f'    pH    {pH:.2f}')
+    if carbon_molality is not None:
+        input_newlines.append(f'    C    {carbon_molality}')
+    if alkalinity is not None:
+        input_newlines.append(f'    Alkalinity    {alkalinity:.8f} as HCO3')
+
+    for k in composition.keys():
+        input_newlines.append(f'    {k}    {composition[k]:.8f}')
+
+    insert_lines_into_file(input_file_path_new, input_newlines, 6)
 
     subprocess.run(['./phreeqc', 'input'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=PHREEQC_path)
 
@@ -125,62 +193,24 @@ if __name__ == '__main__':
         'Mg': 0.0528 * sal,
         'S(6)': 0.0282 * sal,
         'Ca': 0.0103 * sal,
-        'K': 0.0102 * sal
+        'K': 0.0102 * sal,
+        'Si': 0.0000 * sal,
+        'Al': 0.00000 * sal
     }
 
-    # comp: dict[str, float] = {
-    #     'Cl': 0.546,
-    #     'Na': 0.469,
-    #     'Mg': 0.0528,
-    #     'S(6)': 0.0282,
-    #     'Ca': 0.0103,
-    #     'K': 0.0102
-    # }
+    P_surface = 2 * EARTH_ATM
+    T_surface = 293
+    original_C_molality = 0.002
 
-    alk = 1000 * 1e-6
-    C = 0.001
+    # find_partial_pressures(P_surface, 310, comp, carbon_molality=0.1, pH=7)
+    seafloor_equilbrium_v2(P_seafloor=10*1e5, T_seafloor=280, composition=comp, minerals=['Calcite', 'Aragonite'], carbon_molality=0.1, pH=7)
 
-    dT = +3
+    # new_composition, new_alkalinity, new_carbon_molality = seafloor_equilbrium(400 * EARTH_ATM, 275, comp, ['Anorthite'], original_alkalinity, original_C_molality)
 
-    T_bottom = 277
-    T_bottom_new = T_bottom + dT
+    # print(new_composition)
+    # print(new_carbon_molality)
 
-    # T_bottom = 273 + 400
-    # T_bottom_new = 273 + 403
+    # new_composition, new_alkalinity, new_carbon_molality = seafloor_equilbrium(400 * EARTH_ATM, 275, comp, ['Anorthite'], original_alkalinity, original_C_molality)
 
-    T_0 = 293
-    T_0_new = T_0 + dT
-
-    P_floor = 500
-
-    print(f'Alkanlinity : {alk}, C : {C}')
-
-    new_comp, new_alk, new_C = seafloor_equilbrium(P_floor * EARTH_ATM, T_bottom, comp, alkalinity=alk, carbon_molality=C)
-    P_CO2_old, P_H2O = find_partial_pressures(1 * EARTH_ATM, T_0, new_comp, new_alk, new_C)
-
-    print(f'Alkanlinity : {new_alk}, C : {new_C}, P_CO2: {P_CO2_old / EARTH_ATM:.4%}, P_H2O: {P_H2O / EARTH_ATM:.4%}')
-
-    new_comp, new_alk, new_C = seafloor_equilbrium(P_floor * EARTH_ATM, T_bottom, new_comp, alkalinity=new_alk, carbon_molality=new_C)
-    P_CO2, P_H2O = find_partial_pressures(1 * EARTH_ATM, T_0_new, new_comp, new_alk, new_C) # type: ignore
-
-    print(f'Alkanlinity : {new_alk}, C : {new_C}, P_CO2: {P_CO2 / EARTH_ATM:.4%}, P_H2O: {P_H2O / EARTH_ATM:.4%}')
-
-    new_comp, new_alk, new_C = seafloor_equilbrium(P_floor * EARTH_ATM, T_bottom_new, new_comp, alkalinity=new_alk, carbon_molality=new_C)
-    P_CO2, P_H2O = find_partial_pressures(1 * EARTH_ATM, T_0_new, new_comp, new_alk, new_C) # type: ignore
-
-    print(f'!! Alkanlinity : {new_alk}, C : {new_C}, P_CO2: {P_CO2 / EARTH_ATM:.4%}, P_H2O: {P_H2O / EARTH_ATM:.4%} !!')
-
-    new_comp, new_alk, new_C = seafloor_equilbrium(P_floor * EARTH_ATM, T_bottom_new, new_comp, alkalinity=new_alk, carbon_molality=new_C)
-    P_CO2, P_H2O = find_partial_pressures(1 * EARTH_ATM, T_0, new_comp, new_alk, new_C) # type: ignore
-
-    print(f'Alkanlinity : {new_alk}, C : {new_C}, P_CO2: {P_CO2 / EARTH_ATM:.4%}, P_H2O: {P_H2O / EARTH_ATM:.4%}')
-
-    # P_CO2, P_H2O = find_partial_pressures(EARTH_ATM, 290, comp, alkalinity=2223 * 1e-6, carbon_molality=0.002)
-
-    # print(f'{P_CO2 / EARTH_ATM:.4%}')
-    # print(f'{P_H2O / EARTH_ATM:.2%}')
-
-    # alk = reverse_partial_pressure(EARTH_ATM, 290, (400 * 1e-6) * EARTH_ATM, comp, carbon_molality=0.002, find_alkalinity=True)
-
-    # print(alk * 1e6)
-
+    # print(new_composition)
+    # print(new_carbon_molality)
